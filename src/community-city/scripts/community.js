0b4430000c73e1430b97a3b3e1062d8c09c5d03e
dannygit2019 <danny.tran.dba@gmail.com>
2019-11-12 21:27:22 -0700

    adding city and community.js

---
+++ src/community-city/scripts/community.js
@@ -0,0 +1,159 @@
+import { postData } from './apiSetup.js'
+
+
+const url = 'http://localhost:5000/';
+
+
+    export class City {
+        
+        constructor (key, Name, Latitude, Longitude, Population) {
+            
+            this.key = key;
+            this.Name = Name;
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+            this.Population = Population;
+
+        }
+        show() {
+            //return "calgary";
+            let cityAttributes;
+            // cityAttributes = `ID: ${this.key}, City: ${this.Name}, Latitude: ${this.Latitude}, ` + 
+            //                 `Longtitude: ${this.Longitude}, Population: ${this.Population}`;
+            cityAttributes = `City: ${this.Name}, Lat.: ${this.Latitude}, Long.: ${this.Longitude}, Population: ${this.Population}`;
+                            
+            return cityAttributes;
+        }
+
+        moveIn(numberInput) {
+            this.Population += numberInput;
+        }
+        moveOut(numberInput) {
+            this.Population -= numberInput;
+        }
+        howBig() {
+            if (this.Population > 100000) return "City";
+            if (this.Population > 20000) return "Large Town";
+            if (this.Population >= 1000) return "Town";
+            if (this.Population > 100) return "Village";
+            if (this.Population >= 1) return "Hamlet";
+            return "Unknown-Danny's Land";
+        }
+        
+    }
+    export class Community {
+        constructor() {
+            this.CityList=[];  
+        }
+
+        whichSphere(city) {
+            if (city.Latitude > 0) return "Northern Hemisphere";
+            if (city.Latitude < 0) return "Southern Hemisphere";
+            return "Neither Northern nor Southern Hemisphere."
+        }
+
+        createCity(key, Name, Latitude, Longitude, Population) {
+            const newCity = new City(key, Name, Latitude, Longitude, Population);
+            this.CityList.push(newCity);
+            //await this.postData(url + 'add', this.CityList);
+            
+        }
+        
+        deleteCity(cityKey) {
+            let newCityList = this.CityList.filter(function(value){
+                return value.key !== cityKey; 
+            });
+            this.CityList = newCityList;
+            //return this.CityList;
+        }
+        // getMostNorthern() {
+           
+        //     let highestLat = this.cityList.reduce(function(a, b) {
+        //         return Math.max(a.Latitude, b.Latitude);
+        //     });
+        //      return highestLat;
+        // }
+
+        
+    }
+    const functions = {
+       
+        addCard: (node,detail,keyid) => {
+            let newLine=document.createElement("br");
+            let newCard=document.createElement("div");
+            newCard.setAttribute("class", "rightshow");
+            // newCard.textContent=`City ${node.childElementCount}`;
+            //newCard.textContent=`City ${cName}`;
+            
+            node.appendChild(newCard);
+            /////functions.addButtons(newCard);
+            let newDisplayName=document.createElement('p');
+            newDisplayName.textContent=keyid;
+            newCard.appendChild(newDisplayName);
+
+            let newDisplayDetail=document.createElement('p');
+            newDisplayDetail.setAttribute("id", "displayDetail");
+            newDisplayDetail.setAttribute("class", "pdisplay");
+            newDisplayDetail.textContent=detail;
+            newCard.appendChild(newDisplayDetail);
+
+            newCard.appendChild(newLine);
+            
+            let lblMove=document.createElement("label");
+            lblMove.textContent="Move In/Move Out:";
+            lblMove.setAttribute("class","lblmoving");
+            newCard.appendChild(lblMove);
+
+            let txtModify = document.createElement("input");
+            txtModify.setAttribute("type","number");
+            txtModify.setAttribute("class","rightPop");
+            txtModify.setAttribute("id","txtmodifyPop");
+            newCard.appendChild(txtModify);
+
+            
+            let removeCity = document.createElement("button");
+            removeCity.setAttribute("class","btnright");
+            removeCity.setAttribute("id","delete");
+            removeCity.textContent="Delete";
+            
+            newCard.appendChild(removeCity);
+
+            let movingIn = document.createElement("button");
+            movingIn.setAttribute("class","btnright");
+            movingIn.setAttribute("id","moveIn");
+            movingIn.textContent="Move In";
+            
+            newCard.appendChild(movingIn);
+
+            let movingOut = document.createElement("button");
+            movingOut.setAttribute("class","btnright");
+            movingOut.setAttribute("id","moveOut");
+            movingOut.textContent="Move Out";
+            
+            newCard.appendChild(movingOut);
+            
+            let howBig = document.createElement("button");
+            howBig.setAttribute("class","btnright");
+            howBig.setAttribute("id","howBig");
+            howBig.textContent="How Big";
+            
+            newCard.appendChild(howBig);
+
+            let sphere = document.createElement("button");
+            sphere.setAttribute("class","btnright");
+            sphere.setAttribute("id","sphere");
+            sphere.textContent="Hemisphere";
+            
+            newCard.appendChild(sphere);
+
+
+
+            return newCard;
+            
+        },
+
+    
+    };
+
+
+export {functions};
