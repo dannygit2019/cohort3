863acbbaf0a2e66c7191676e2c93e55f6d018554
dannygit2019 <danny.tran.dba@gmail.com>
2019-12-17 18:33:35 -0700

    App.css revised

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -7,10 +7,17 @@ import tictactoe from './images/tictactoe.svg';
 import bank from './images/bank.svg';
 import city from './images/city.svg'
 import TicTacToe from './TicTacToe';
+import link from './images/link.svg';
 import { Square,Board,  Game} from './components/Game';
 import {AccountPage} from './components/AccountReact/CreateAccount';
+import {CityPage} from './components/ReactCity/CreateCity';
+import { City, Community } from './components/ReactCity/CommunityPOJO';
+import { postData } from './components/ReactCity/apiSetup'
+import { fetchToServer } from './components/ReactCity/fetchCity'
 
+//import { BarChart } from './BarChart'
 
+const url = 'http://localhost:5000/';
 
 class App extends Component {
   	constructor() {
@@ -20,8 +27,15 @@ class App extends Component {
 		whichHomePage: 0,
 		//controller: new AccountController()
 		}
-
-  	}
+		this.newCommunity = new Community();
+	 }
+	// loadData = async() => {
+	// 	await fetchToServer.loadData(this.newCommunity);
+	// }
+	// componentDidMount() {
+	// 	this.loadData();
+	// 	console.log(this.newCommunity.CityList);
+	//  }  
 	imageclicked(imageIndex) {
 		let image =  document.getElementById(this.state.whichHomePage);
 		image.style.filter="grayscale(100%)";
@@ -41,16 +55,30 @@ class App extends Component {
 		
 		if (this.state.whichHomePage === 0){
 			return (
-				
 				<div className="App"> 
+					{/* <div className="App-header">
+						<img src={logo} className="App-logo" alt="logo" /> */}
+						{/* <div className="App" > */}
+						<h2 className="h22"><span className="spanshadow">Welcome to React</span></h2>
+						<div className="marquee"><p></p></div>
 					<div className="App-header">
 						<img src={logo} className="App-logo" alt="logo" />
-						<h2>Welcome to React</h2>
+						<p className="App-intro">
+							To get started, edit <code>src/App.js</code> and save to reload.
+						</p>
 					</div>
-					<p className="App-intro">
-						To get started, edit <code>src/App.js</code> and save to reload.
-					</p>	
 				</div>
+
+
+				// <div className="App"> 
+				// 	<div className="App-header">
+				// 		<img src={logo} className="App-logo" alt="logo" />
+				// 		<h2>Welcome to React</h2>
+				// 	</div>
+				// 	<p className="App-intro">
+				// 		To get started, edit <code>src/App.js</code> and save to reload.
+				// 	</p>	
+				// </div>
 				
 			);
 		}
@@ -72,29 +100,27 @@ class App extends Component {
 			)
 		}
 		if (this.state.whichHomePage === 3){
+			
 			return (
-				<div className="App"> 
-					<div className="App-header">
-						<img src={logo} className="App-logo" alt="logo" />
-						<h2>Welcome to React</h2>
-					</div>
-					<p className="App-intro">
-						To get started, edit <code>src/App.js</code> and save to reload.
-					</p>	
+				<div> 
+					
+					<CityPage />
 				</div>
 			)
 		}
 	}
 	
 	render() {
-		let array = ["react", "tictactoe", "bank", "city"];
+	
+		let array = ["react", "tictactoe", "bank", "city", "link"];
         let images = array.map((image,i) => {
 		   return <img key={image} tabIndex={i} id={i} src={require(`./images/${image}.svg`)} alt="" className={`App-logo modify i${image}`} onClick={(e) => {
 			this.imageclicked(e.target.tabIndex)
-			this.changeColor(e.target.tabIndex)}}/>
+			this.changeColor(e.target.tabIndex)
+			}}/>
 		});
         return (
-			<div className="bigWndow">
+			<div className="bigWndow" style={{marginBottom:"250px"}}>
             <div className="App imagesClass" id="imagesID" >	
 				{ images }
 				{this.displayPage()}
