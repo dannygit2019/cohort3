b1317ddbdc35270638ede08bce74c16854657a04
dannygit2019 <danny.tran.dba@gmail.com>
2020-01-22 11:07:39 -0700

    checking lat and long added

--- react-02/src/components/ReactCity/CreateCity.js
+++ react-02/src/components/ReactCity/CreateCity.js
@@ -37,15 +37,15 @@ class CityPage extends Component {
 	componentDidMount = async () => {
 		let data = await postData(url + 'all');
 		if (data.length > 0) {
-			alert("Data found on the server and will be loaded.");
+			//alert("Data found on the server and will be loaded.");
 			await fetchToServer.loadData(this.newCommunity);
 			this.uniqueKey = this.newCommunity.CityList[this.newCommunity.CityList.length - 1].key;
 			this.setState({
 				keyCard: this.uniqueKey + 1,
 			});
-		} else {
-			alert("Sorry we currently don't have data on the server!");
-		}
+		}// else {
+			//alert("Sorry we currently don't have data on the server!");
+		//}
 	}
 
 	handleChange = (event) => {
@@ -58,15 +58,64 @@ class CityPage extends Component {
 	handleSubmit = async (event) => {
 		event.preventDefault();
 		let existList = false;
+	
 		if (this.newCommunity.CityList.length > 0) { existList = true };
 		switch (event.target.name) {
 			case "btnCreate":
+				let checkLat=false;
+				let checkLong=false;
 				let duplicateCity = false;
+				if (this.state.latitude < -90 || this.state.latitude > 90) {
+					checkLat=true;
+					// this.setState({
+					// 	error: "Invalid number. Latitude is between -90 and 90!",
+					// 	latitude: ""
+					// })
+					// return;
+				}
+				if (this.state.longitude < -180 || this.state.longitude > 180) {
+					checkLong=true;
+					// this.setState({
+					// 	error: "Invalid number. Longitude is between -180 and 180!",
+					// 	longitude: ""
+					// })
+					// return;
+				}
+				if (checkLat === true && checkLong===true) {
+					this.setState({
+						error: "Invalid Lat/Long. LAT: (Min.: -90 and Max.: 90). LONG: (Min.: -180 and Max.: 180).",
+						longitude: "",
+						latitude: ""
+					})
+					return;
+				}
+				if (checkLat === true || checkLong===true) {
+					if (checkLat===true) {
+						this.setState({
+							error: "Invalid Lat. Min.: -90 and Max.: 90",
+							latitude: ""
+						})
+						return;
+					} else {
+						this.setState({
+							error: "Invalid Long. Min.: -180 and Max.: 180.",
+							longitude: ""
+						})
+						return;
+					}
+				}
+
 				if (this.newCommunity.CityList.length > 0) {
 					let findLat = this.newCommunity.CityList.filter((lat) => {
 						return Number(lat.Latitude) === Number(this.state.latitude);
 					})
-					if (findLat.length === 1) { duplicateCity = true }
+					let findLong = this.newCommunity.CityList.filter((longi) => {
+						return Number(longi.Longitude) === Number(this.state.longitude);
+					})
+					console.log(findLat.length, findLong.length)
+					// if (findLat.length === 1 && findLong.length === 1) { duplicateCity = true }
+					// else { duplicateCity = false }
+					if (findLat.length > 0 && findLong.length > 0) { duplicateCity = true }
 					else { duplicateCity = false }
 				// the code below is original before making changes
 					// let findCity = this.newCommunity.CityList.filter((city) => {
@@ -78,17 +127,11 @@ class CityPage extends Component {
 				}
 				if (duplicateCity === true) {
 					this.setState({
-						error: "Latitude already existed. Please try again!",
+						error: "Latitude and Longitude already existed. Please try again!",
 						latitude: ""
-						// error: "City already existed. Please try again!",
-						// cityName: ''
 					})
 				} else {
-					// if (this.state.population === "") {
-					// 	this.setState({
-					// 		error: "It",
-					// 	})
-					// } else {
+
 					if (this.state.cityName !== "" && this.state.latitude !== "" && this.state.longitude !== "") {
 						let city = { key: this.state.keyCard, Name: (this.state.cityName).toUpperCase(), Latitude: Number(this.state.latitude), Longitude: Number(this.state.longitude), Population: Number(this.state.population) };
 						this.newCommunity.createCity(this.state.keyCard, (this.state.cityName).toUpperCase(), Number(this.state.latitude), Number(this.state.longitude), Number(this.state.population));
@@ -190,9 +233,9 @@ class CityPage extends Component {
 					keyCard: this.state.keyCard + 1,
 				});
 			}
-		} else {
-			alert("Deleting the account has been CANCELED.");
-		}
+		} //else {
+			//alert("Deleting the account has been CANCELED.");
+		//}
 	}
 
 	removeErrorMessage = () => {
