37a811c160dcd1708f9d59d15de982620f098441
dannygit2019 <danny.tran.dba@gmail.com>
2020-02-04 19:42:08 -0700

    syntax updated

--- src/python/syntax.py
+++ src/python/syntax.py
@@ -1,4 +1,4 @@
-# --------------- Copy this section into your code syntax.py as comments --------
+# --------------- Copy this section into your code syntax.js as comments --------
 
 # define attributes / variables
 # number
@@ -19,18 +19,65 @@ def defineArray():
 # dictionary / objects
 def defineDictionary(dictionaryInput):
     return dictionaryInput
+
+class MyClass:
+  name="in Class"
 # undefined
+def defineUndefined():
+    try:
+        return(varNotDefined)
+    except:
+        return("undefined") 
+
 # sample if / else
+def ifElseSample(n1,n2):
+    message=""
+    if n1 > n2:
+        message=f"{n1}>{n2}"
+    else:
+        message=f"{n1}<{n2}"
+    return message
 # functions
     # parameters # returns
+
+def acceptParameters(p1,p2):
+    return f"two parameters are {p1} and {p2}."
 # arrays
     # add to the front
     # add to the end
     # update values
+def arrAddToFront(newItem):
+    arr=['B','C']
+    arr.insert(0,newItem)
+    return arr
+def arrAddToLast(newItem):
+    arr=['A','B']
+    arr.append(newItem)
+    return arr
+def arrUpdate(newItem):
+    arr=['A','B']
+    arr[0]=newItem
+    return arr
 # loops 
     # for/in
+def testForIn(arrInput):
+    total=0
+    for anElement in arrInput:
+        total+=anElement
+    return total
     # while
+def testWhile(arrInput):
+    total=0
+    i=0
+    print(len(arrInput))
+    while i < len(arrInput):
+        total+=arrInput[i]
+        i=i+1
+    return total
+    
 # Objects / Dictionaries
     # declare object
     # lookup key to retrieve value
+def defineObject(objectInput):
+    return objectInput["name"]
 # --------------- Copy ends here --------
--- src/python/syntax_test.py
+++ src/python/syntax_test.py
@@ -1,9 +1,22 @@
 import syntax
 
-def testSyntax():
+def testNum():
     assert syntax.defineNum(1)==1
     assert syntax.defineString("dd")=="dd"
     assert syntax.defineBoolean(3,1)==True
     assert syntax.defineBoolean(3,5)==False
     assert syntax.defineArray()==['A','B']
     assert syntax.defineDictionary({"name":"Tom", "Age":20})["name"]=="Tom"
+    # create an object f the class MyClass
+    myObject=syntax.MyClass()
+    assert myObject.name=="in Class"
+    assert syntax.defineUndefined()=="undefined"
+    assert syntax.ifElseSample(5,2)=="5>2"
+    assert syntax.ifElseSample(2,5)=="2<5"
+    assert syntax.acceptParameters(1,2)=="two parameters are 1 and 2."
+    assert syntax.arrAddToFront('A')==['A','B','C']
+    assert syntax.arrAddToLast('C')==['A','B','C']
+    assert syntax.arrUpdate("A updated to UPDATED")==["A updated to UPDATED","B"]
+    assert syntax.testForIn([1,4])==5
+    assert syntax.testWhile([1,4,5])==10
+    assert syntax.defineObject({"name":"Dan", "hobby":"sport"})=="Dan"
