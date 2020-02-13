1e0f4fcdb5ec5f0d72b9fa58647e6b59075fd847
dannygit2019 <danny.tran.dba@gmail.com>
2020-02-12 20:25:48 -0700

    reading directory added

--- src/python/comp220/comp220.py
+++ src/python/comp220/comp220.py
@@ -1,11 +1,11 @@
-import os
+import os, csv
 
 # reading a file
 def readAFile():
     count=0
     countElse=0
     countCharacters=0
-    sampleFile=open('/home/danny/code/cohort3/src/javascript/syntax.js','r')
+    sampleFile=open('/home/danny/code/cohort3/src/python/comp220/syntaxJavascript.js','r')
     if sampleFile.mode == 'r':
         byLines=sampleFile.readlines()
         for countline in byLines:
@@ -13,3 +13,32 @@ def readAFile():
             if 'else' in countline:
                 countElse=countElse + 1
         return f"Number of Lines: {count}. Number of Else: {countElse}."
+
+# count number of characters
+def countChar():
+    countCharacters=0
+    sampleFile=open('/home/danny/code/cohort3/src/python/comp220/syntaxJavascript.js',"r")
+    if sampleFile.mode == 'r':
+        byAll=sampleFile.read()
+        for characters in byAll:
+            countCharacters=countCharacters +1
+        return f"Number of Characters: {countCharacters}."
+
+# Reading directories
+def readDirectories(directoryInput):
+    fName=""
+    statInfo=""
+    fSize=0
+    totalSize=0
+    # basepath = '/home/danny/code/cohort3/src/copy/'
+    basepath=directoryInput
+    for entry in os.listdir(basepath):
+        if os.path.isfile(os.path.join(basepath, entry)): # ENTRY is a file name
+            # statInfo=os.stat('/home/danny/code/cohort3/src/copy/' + entry) # to get stat of a file
+            statInfo=os.stat(basepath + entry) # to get stat of a file
+            fSize=statInfo.st_size # to determine size of a file
+            totalSize+=fSize
+            fName= fName + ' ' + entry + ' '+ f"[{str(fSize)}]"        
+    fileNames=fName.strip()
+    detail=f"List of Files with Size:{fName}. Total Size of the Directory is: {totalSize}."
+    return detail
--- src/python/comp220/comp220_test.py
+++ src/python/comp220/comp220_test.py
@@ -1,5 +1,6 @@
 import comp220
 def testing():
 #   test reading a file - count number of lines, else statements and characters
-    assert comp220.readAFile()=="Number of Lines: 135. Number of Else: 2."
-    
+    assert comp220.readAFile()=="Number of Lines: 129. Number of Else: 2."
+    assert comp220.countChar()=="Number of Characters: 2790."
+    assert comp220.readDirectories('/home/danny/code/cohort3/src/copy/')=="List of Files with Size: community.test.js [4418] README.md [80668] community.js [7067]. Total Size of the Directory is: 92153."
